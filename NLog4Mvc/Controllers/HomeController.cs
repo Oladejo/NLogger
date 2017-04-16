2778e5f12b7898401f51785eb1c393bb6c0a99a7
Oladejo <bab.oladejo@student.oauife.edu.ng>
2017-04-16 18:21:41 +0100

    Initialised

---
+++ NLog4Mvc/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using System;
+using System.Web.Mvc;
+
+namespace NLog4Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            try
+            {
+                int x = 0;
+                int y = 5;
+                int z = y / x;
+            }
+            catch (Exception ex)
+            {
+                //logger.Trace(ex);
+                logger.ErrorException("Error occured in Home controller Index Action", ex);
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
