ea2d71281f47f218bc905e79cd12b36aba8b6372
Oladejo <bab.oladejo@student.oauife.edu.ng>
2017-04-16 18:27:20 +0100

    Initialised

---
+++ NLog4Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NLog4Mvc.MvcApplication" Language="C#" %>
---
+++ NLog4Mvc/Models/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NLog4Mvc.Models
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public string CallSite { get; set; }
+        public string DateTime { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string MachineName { get; set; }
+        public string Username { get; set; }
+        public string ErrorSource { get; set; }
+        public string ErrorClass { get; set; }
+        public string ErrorMethod { get; set; }
+        public string ErrorMessage { get; set; }
+        public string InnerErrorMessage { get; set; }
+        public string Exception { get; set; }
+        public string StackTrace { get; set; }
+        public string Thread { get; set; }
+    }
+}
